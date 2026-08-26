683ee2d97dccfc617881fb6e41899c849687824f
pranay8873 <pranaypuvaati@gmail.com>
2026-08-26 09:49:49 +0530

    PD : merge in pandas

--- pandas/merging.py
+++ pandas/merging.py
@@ -1,25 +1,5 @@
 import pandas as pd
 
-sales = pd.DataFrame({
-    "Order_ID": ["O101","O102","O103","O104"],
-    "Customer_ID": ["C01","C02","C01","C04"],
-    "Product_ID": ["P01","P02","P03","P01"],
-    "Quantity": [2,1,3,1],
-    "Revenue": [1200,800,2100,600]
-})
-
-customers = pd.DataFrame({
-    "Customer_ID": ["C01","C02","C03","C04"],
-    "Customer_Name": ["Arun","Priya","Rahul","Sneha"],
-    "City": ["Hyderabad","Chennai","Bengaluru","Hyderabad"],
-    "Segment": ["Retail","Corporate","Retail","Corporate"]
-})
-
-products = pd.DataFrame({
-    "Product_ID": ["P01","P02","P03"],
-    "Product": ["Laptop","Monitor","Phone"],
-    "Category": ["Electronics","Electronics","Mobile"]
-})
 jan = pd.DataFrame({
     "Order_ID": ["O101","O102"],
     "Revenue": [1200,800]
@@ -42,3 +22,25 @@ b=pd.DataFrame({"roll_no":[66,99,90,92],"sec":['a','b','c','d']})
 
 com=pd.concat([a,b],axis=1)
 print(com)
+sales = pd.DataFrame({
+    "Order_ID": ["O101","O102","O103","O104"],
+    "Customer_ID": ["C01","C02","C01","C04"],
+    "Product_ID": ["P01","P02","P03","P01"],
+    "Quantity": [2,1,3,1],
+    "Revenue": [1200,800,2100,600]
+})
+
+customers = pd.DataFrame({
+    "Customer_ID": ["C01","C02","C03","C04"],
+    "Customer_Name": ["Arun","Priya","Rahul","Sneha"],
+    "City": ["Hyderabad","Chennai","Bengaluru","Hyderabad"],
+    "Segment": ["Retail","Corporate","Retail","Corporate"]
+})
+
+products = pd.DataFrame({
+    "Product_ID": ["P01","P02","P03"],
+    "Product": ["Laptop","Monitor","Phone"],
+    "Category": ["Electronics","Electronics","Mobile"]
+})
+mer=pd.merge(sales,customers,on="Customer_ID",how='left')
+print(mer)
